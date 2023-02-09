947b6357d25c2f1bf22bd6d0658dc5ca02078f6e
Kurt Hill <kurtis.hill212@gmail.com>
2023-02-09 08:14:44 -0500

    library deserializer test with some fixes on the book model

--- library_app/lib/logic/models/book.dart
+++ library_app/lib/logic/models/book.dart
@@ -4,7 +4,7 @@ class Book extends Equatable {
   final String title;
   final String author;
   final String publisher;
-  final DateTime publicationDate;
+  final String publicationDate;
   final String genre;
   final List<String> tags;
   final String imageUrl;
@@ -23,9 +23,11 @@ class Book extends Equatable {
       : title = json['title'],
         author = json['author'],
         publisher = json['publisher'],
-        publicationDate = json['publicationDate'],
+        publicationDate = json['publication_date'],
         genre = json['genre'],
-        tags = json['tags'],
+        tags = (json['tags'] as List<dynamic>)
+            .map((tag) => tag.toString())
+            .toList(),
         imageUrl = json['image'];
 
   @override
